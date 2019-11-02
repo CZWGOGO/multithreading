bab3d20ab8dc222c77c31dbd0e4ec6b1b0143a3d
电子牙 <925276703@qq.com>
2019-11-02 23:49:39 +0800

    synchronized 解决同步问题

---
+++ src/multiplethread/TestThread9.java
@@ -0,0 +1,91 @@
+package multiplethread;
+
+import charactor.Hero;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by CZWGOGO on 2019/11/2 22:46
+ */
+public class TestThread9 {
+    public String now() {
+        return new SimpleDateFormat("HH:mm:ss").format(new Date());
+    }
+
+    public static void main(String[] args) {
+        final Object someObject = new Object();
+
+        final Hero gareen = new Hero();
+        gareen.name = "盖伦";
+        gareen.hp = 10000;
+
+        int n = 10000;
+
+        Thread[] addThreads = new Thread[n];
+        Thread[] reduceThreads = new Thread[n];
+
+        for (int i = 0; i < n; i++) {
+            Thread t = new Thread(){
+                public void run(){
+
+                    //任何线程要修改hp的值，必须先占用someObject
+                    synchronized (someObject) {
+                        gareen.recover();
+                    }
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            };
+            t.start();
+            addThreads[i] = t;
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            Thread t = new Thread(){
+                public void run(){
+                    //任何线程要修改hp的值，必须先占用someObject
+                    synchronized (someObject) {
+                        gareen.hurt();
+                    }
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            };
+            t.start();
+            reduceThreads[i] = t;
+        }
+
+        for (Thread t : addThreads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        for (Thread t : reduceThreads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        System.out.printf("%d个增加线程和%d个减少线程结束后%n盖伦的血量是 %.0f%n", n,n,gareen.hp);
+
+    }
+
+}
